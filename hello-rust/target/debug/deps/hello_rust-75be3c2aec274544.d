bedf6035af7f8975637bfc8e67ae1a1e85136e3e
meichan2 <yhtk34@yahoo.co.jp>
2021-03-24 11:28:53 +0000

    testhello

---
+++ hello-rust/target/debug/deps/hello_rust-75be3c2aec274544.d
@@ -0,0 +1,5 @@
+/home/runner/InfatuatedJovialLibrary/test_sq/hello-rust/target/debug/deps/hello_rust-75be3c2aec274544: src/main.rs
+
+/home/runner/InfatuatedJovialLibrary/test_sq/hello-rust/target/debug/deps/hello_rust-75be3c2aec274544.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/runner/InfatuatedJovialLibrary/test_sq/hello-rust/target/debug/hello-rust: /home/runner/InfatuatedJovialLibrary/test_sq/hello-rust/src/main.rs
